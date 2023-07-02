a7adfc8a71a402540c43baf5f7094cc65c7b1d8a
R M Phani Kumar <38785107+phani-aipod@users.noreply.github.com>
2023-07-03 02:09:49 +0530

    added server code app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def get_bot_response():
     userText = request.args.get('msg')
     return get_response(userText)
 if __name__ == "__main__":
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
